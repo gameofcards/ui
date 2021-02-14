a68fe2d4c610d9d73f30c5e5374fa2909ad61aac
Michael Dito <mcdito13@gmail.com>
2021-02-13 21:03:42 -0800

    configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Game from './pages/Game';
+import React from 'react';
 
-export default () =>
+export default () => (
   <Switch>
     <Route path="/" component={Home} exact>
       <Home />
     </Route>
-    <Route path="/games/:id" component={Game}>
-      <Game />
-    </Route>
   </Switch>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
@@ -14,12 +14,12 @@ const store = {
     test: 'hello world omg',
     theNumber: 42,
     todos: [
-      {id: 1, name: 'bob', todo: 'run', __typename: 'todo'},
-      {id: 2, name: 'bill', todo: 'sit', __typename: 'todo'},
-      {id: 3, name: 'buck', todo: 'walk', __typename: 'todo'}
-    ]
-  }
-}
+      { id: 1, name: 'bob', todo: 'run', __typename: 'todo' },
+      { id: 2, name: 'bill', todo: 'sit', __typename: 'todo' },
+      { id: 3, name: 'buck', todo: 'walk', __typename: 'todo' },
+    ],
+  },
+};
 const cache = new InMemoryCache();
 
 persistCache({
@@ -27,27 +27,27 @@ persistCache({
   storage: window.localStorage as PersistentStorage<PersistedData<NormalizedCacheObject>>,
 });
 
-const client = new ApolloClient({ 
+const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
   cache,
   resolvers: {
     Query: {
       derivedString: () => {
-        return 'derived'
+        return 'derived';
       },
       derivedObj: () => ({
         a: 'a',
-        b: 'b', 
-        c: 'c'
+        b: 'b',
+        c: 'c',
       }),
       derivedArray: () => {
-        return [0,1,2]
-      }
+        return [0, 1, 2];
+      },
     },
     Mutation: {
       initCache: (_root, _args, { cache }) => {
         cache.writeData(store);
-        console.log('inittin cache')
+        console.log('inittin cache');
         return null;
       },
     },
